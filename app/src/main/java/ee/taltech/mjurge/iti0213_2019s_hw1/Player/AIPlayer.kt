72d120065c9d5100392a11f07013d337d9282bda
mjurge <mjurge@ttu.ee>
2020-03-07 22:51:25 +0200

    Testing pipeline

--- app/src/main/java/ee/taltech/mjurge/iti0213_2019s_hw1/Player/AIPlayer.kt
+++ app/src/main/java/ee/taltech/mjurge/iti0213_2019s_hw1/Player/AIPlayer.kt
@@ -2,16 +2,13 @@ package ee.taltech.mjurge.iti0213_2019s_hw1.Player
 
 import ee.taltech.mjurge.iti0213_2019s_hw1.GameSquare
 import ee.taltech.mjurge.iti0213_2019s_hw1.InterfaceGameSquare
-import ee.taltech.mjurge.iti0213_2019s_hw1.board.InterfaceBoard
-import ee.taltech.mjurge.iti0213_2019s_hw1.board.InterfaceMove
-import ee.taltech.mjurge.iti0213_2019s_hw1.board.InterfacePosition
-import ee.taltech.mjurge.iti0213_2019s_hw1.board.Position
+import ee.taltech.mjurge.iti0213_2019s_hw1.board.*
 import java.io.Serializable
 
 class AIPlayer<T: InterfaceGameSquare, U: InterfacePosition>(private var playerString: String):
     InterfacePlayer<T, U>, Serializable {
     override suspend fun getMove(board: InterfaceBoard<T, U>): InterfaceMove<U> {
-
+        return Move(Position(1, 2), Position(2,3)) as InterfaceMove<U>
     }
 
     override fun onUserClickedLocation(location: U) {
